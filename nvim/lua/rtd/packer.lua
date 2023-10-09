ecc7d6cb4112c069c85d34644250af909c1b4544
Ryan H <30601201+ryanthedev@users.noreply.github.com>
2023-10-09 11:07:05 -0500

    tmux goodies

--- nvim/lua/rtd/packer.lua
+++ nvim/lua/rtd/packer.lua
@@ -70,7 +70,20 @@ return require("packer").startup(function(use)
     end,
   })
 
-  use('christoomey/vim-tmux-navigator')
+use { 'alexghergh/nvim-tmux-navigation', config = function()
+        require'nvim-tmux-navigation'.setup {
+            disable_when_zoomed = true, -- defaults to false
+            keybindings = {
+                left = "<C-h>",
+                down = "<C-j>",
+                up = "<C-k>",
+                right = "<C-l>",
+                last_active = "<C-Backspace>",
+                next = "<C-n>",
+            }
+        }
+    end
+}
 
   -- DAP
   use({
