bc955904d588b771a363dd6f1e32edf72f90add6
Ryan H <30601201+ryanthedev@users.noreply.github.com>
2023-10-23 06:39:03 -0500

    polybar

---
+++ polybar/randy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+# This is an example of how to launch polybar on every connected monitor
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
