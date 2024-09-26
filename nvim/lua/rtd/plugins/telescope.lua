e8b4266ecfa1655a8de25f861cd9ff588e7daef9
Ryan H <30601201+ryanthedev@users.noreply.github.com>
2024-09-26 09:56:01 -0500

    enhancements

--- nvim/lua/rtd/plugins/telescope.lua
+++ nvim/lua/rtd/plugins/telescope.lua
@@ -1,15 +1,15 @@
 return {
   'nvim-telescope/telescope.nvim',
-  tag = '0.1.8',
+  commit = '4367e05',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function ()
     require('telescope').setup{
       defaults = {
         layout_strategy = 'vertical',
         layout_config = { height = 0.95, width = 0.99 },
-        path_display = {
-          shorten = 2
-        },
+        -- path_display = {
+        --   shorten = 2
+        -- },
       },
     }
   end
