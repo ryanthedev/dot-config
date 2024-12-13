14becff9c1b744d01e71e4c3ca4f34eeeea319ef
Ryan <rtd>
2024-12-13 05:23:59 -0600

    doing the nerds work

--- nvim/after/plugin/autocommands.lua
+++ nvim/after/plugin/autocommands.lua
@@ -39,3 +39,11 @@ local autoCommands = {
 }
 
 -- M.nvim_create_augroups(autoCommands)
+--
+--
+vim.api.nvim_create_autocmd("FileType",  {
+      pattern = { "json" },
+      callback = function()
+        vim.api.nvim_set_option_value("formatprg", "jq", { scope = 'local' })
+      end,
+})
--- nvim/lua/rtd/plugins/linter.lua
+++ nvim/lua/rtd/plugins/linter.lua
@@ -2,15 +2,16 @@ return {
   'mfussenegger/nvim-lint',
   config = function()
     local lint = require('lint')
-    lint.inters_by_ft = {
+    lint.linters_by_ft = {
       lua = {
       }
     }
 
-
     vim.api.nvim_create_autocmd({ "BufWritePost" }, {
       callback = function()
-        lint.try_lint()
+        if lint.linters_by_ft[vim.bo.filetype] then
+          lint.try_lint()
+        end
       end,
     })
   end
