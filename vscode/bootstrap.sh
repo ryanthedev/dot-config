643842128fdd1bb6b47fa4b5a947fe42853c9e7a
Ryan <rtd>
2025-02-17 04:25:17 -0600

    work

--- vscode/bootstrap.sh
+++ vscode/bootstrap.sh
@@ -1,3 +1,4 @@
 ln -sf "$HOME/.config/vscode/User/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
+ln -sf "$HOME/.config/vscode/User/keybindings.json" "$HOME/Library/Application Support/Code/User/keybindings.json"
 
 cat $HOME/.config/vscode/extensions | xargs -L 1 code --install-extension
