4064610f5087c3f515c5d4a7e0871d8e701572cd
Ryan H <30601201+ryanthedev@users.noreply.github.com>
2025-02-20 11:08:17 -0600

    fzzzzz

--- nvim/lua/rtd/plugins/telescope.lua
+++ nvim/lua/rtd/plugins/telescope.lua
@@ -1,16 +1,24 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  commit = '4367e05',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function ()
-    require('telescope').setup{
-      defaults = {
-        layout_strategy = 'vertical',
-        layout_config = { height = 0.95, width = 0.99 },
-        -- path_display = {
-        --   shorten = 2
-        -- },
-      },
-    }
-  end
+  {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    build = 'make'
+  },
+  {
+    'nvim-telescope/telescope.nvim',
+    commit = '4367e05',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function ()
+      require('telescope').setup{
+        defaults = {
+          layout_strategy = 'vertical',
+          layout_config = { height = 0.95, width = 0.99 },
+          -- path_display = {
+          --   shorten = 2
+          -- },
+        },
+      }
+
+      require('telescope').load_extension('fzf')
+    end
+  }
 }
