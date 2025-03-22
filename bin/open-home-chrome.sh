aab36576c95a4d1fbed2dd5552f460219214354e
Ryan <rtd>
2025-03-22 05:37:28 -0500

    chrome

---
+++ bin/open-home-chrome.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open Home Chrome
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+open -na "Google Chrome" --args --profile-directory="Profile 1" --new-window
+
